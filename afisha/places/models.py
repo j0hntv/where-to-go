804295c3a1113dea993465a4d23197b437b560b8
J0hn <j0hntvdesign@gmail.com>
2020-06-05 09:34:17 +0300

    Add Place model

--- afisha/places/models.py
+++ afisha/places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField('Title', max_length=200)
+    description_short = models.TextField('Short description', blank=True)
+    description_long = models.TextField('Long description', blank=True)
+    lat = models.FloatField('Latitude')
+    lon = models.FloatField('Longitude')
+
+    def __str__(self):
+        return self.title
