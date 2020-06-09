51f8c0896197e19619720576e7000eeafef4d181
J0hn <j0hntvdesign@gmail.com>
2020-06-09 09:13:50 +0300

    Add list display to Place model

--- afisha/places/admin.py
+++ afisha/places/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Place, Image
 
 
-admin.site.register(Place)
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'lat', 'lon', 'description_short')
+
+
 admin.site.register(Image)
