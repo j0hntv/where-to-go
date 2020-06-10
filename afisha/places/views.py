40b454035366c8ee59c9f9e2b927dbc7eede1d1b
J0hn <j0hntvdesign@gmail.com>
2020-06-10 09:06:23 +0300

    Generate Place GeoJSON from database

--- afisha/places/views.py
+++ afisha/places/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from places.models import Place
 
 
 def index(request):
-    return render(request, 'index.html')
+    places = Place.objects.all()
+    features = []
+    for place in places:
+        features.append(
+            {
+                'type': 'Feature',
+                'geometry': {'type': 'Point', 'coordinates': [place.longitude, place.latitude]},
+                'properties': {'title': place.title, 'placeId': place.slug, 'detailsUrl': ''}
+            }
+        )
+        
+    context = {
+        'places_geojson': {
+            'type': 'FeatureCollection',
+            'features': features
+        }
+        
+    }
+
+    return render(request, 'index.html', context)
