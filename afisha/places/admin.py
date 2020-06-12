2203566541a9f7d12d7f7c49a5354bf6216f064f
J0hn <j0hntvdesign@gmail.com>
2020-06-12 17:02:41 +0300

    Add upload image from place admin editing page

--- afisha/places/admin.py
+++ afisha/places/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageInLine(admin.StackedInline):
+    model = Image
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'description_short',)
     prepopulated_fields = {'slug': ('title',)}
+    inlines = (ImageInLine, )
 
 
 admin.site.register(Image)
