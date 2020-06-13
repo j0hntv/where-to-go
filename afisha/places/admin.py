aa49d18a4685826ca2cbe7733efd0b456950231b
J0hn <j0hntvdesign@gmail.com>
2020-06-13 12:55:03 +0300

    Add image preview in Place admin panel

--- afisha/places/admin.py
+++ afisha/places/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Place, Image
 
 
-class ImageInLine(admin.StackedInline):
+class ImageInLine(admin.TabularInline):
     model = Image
+    readonly_fields = ('get_preview',)
+    list_display = ('image', 'get_preview')
+    
+    def get_preview(self, obj):
+        url = obj.image.url
+        return format_html(f'<img src="{url}" width="150" />')
 
 
 @admin.register(Place)
