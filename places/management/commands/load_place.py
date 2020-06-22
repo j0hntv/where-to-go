0895878ce131c99d4b0d432b6e0e1cd67f290d6e
J0hn <j0hntvdesign@gmail.com>
2020-06-22 23:37:30 +0300

    Create a custom management command for load place info from JSON

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,52 @@
+import requests
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+from places.models import Place, Image
+
+
+def get_json(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_raw_image(image_urls):
+    for image_url in image_urls:
+        response = requests.get(image_url)
+        response.raise_for_status()
+        name = image_url.split('/')[-1]
+        yield name, response.content
+
+
+def create_place(json_url):
+    json = get_json(json_url)
+    image_urls = json['imgs']
+    
+    place_object, is_created = Place.objects.get_or_create(
+                title=json['title'],
+                slug=slugify(json['title'], allow_unicode=True),
+                description_short=json['description_short'],
+                description_long=json['description_long'],
+                latitude=json['coordinates']['lat'],
+                longitude=json['coordinates']['lng'],
+            )
+
+    if not is_created:
+        return
+
+    for image in get_raw_image(image_urls):
+        name, content = image
+        image_object = Image.objects.create(place=place_object)
+        image_object.image.save(name, ContentFile(content), save=True)
+
+
+class Command(BaseCommand):
+    help = 'Load places from JSON files to database'
+
+    def add_arguments(self, parser):
+        parser.add_argument('url', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        for url in options['url']:
+            create_place(url)
